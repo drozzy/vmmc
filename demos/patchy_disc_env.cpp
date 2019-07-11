74302c207f5c09b1054a63d3851cb6c1a814bdb2
Andriy Drozdyuk <andriy@drozdyuk.com>
2019-07-11 12:16:33 -0400

    SegFault

--- demos/patchy_disc_env.cpp
+++ demos/patchy_disc_env.cpp
@@ -30,6 +30,7 @@
 #endif
 
 class PatchyDiscEnv {
+private:
     // Simulation parameters.
     unsigned int dimension = 2;                     // dimension of simulation box
     unsigned int nParticles = 1000;                 // number of particles
@@ -39,16 +40,21 @@ class PatchyDiscEnv {
     double baseLength;                              // base length of simulation box
     unsigned int maxInteractions = 3;               // maximum number of interactions per particle (number of patches)
 
-    vmmc::VMMC *vmmc;
-    int width, height;
-public:
-    PatchyDiscEnv(int a, int b){
-        width = a;
-        height = b;
+    std::vector<Particle> particles = std::vector<Particle>(nParticles);    // particle container
 
+    CellList cells;                                 // cell list
+
+    PatchyDisc *patchyDisc;
+    // Initialise input/output class,
+    InputOutput io;
+
+    vmmc::VMMC *vmmc;
+public:
+    PatchyDiscEnv(){
+        std::cout << "Constructor entered.\n";
         // Data structures.
-        std::vector<Particle> particles(nParticles);    // particle container
-        CellList cells;                                 // cell list
+        
+        
         bool isIsotropic[nParticles];
 
         // Resize particle container.
@@ -63,11 +69,7 @@ public:
             boxSize.push_back(baseLength);
 
         // Initialise simulation box object.
-
         Box box(boxSize);
-
-        // Initialise input/output class,
-        InputOutput io;
 
         // Create VMD script.
         io.vmdScript(boxSize);
@@ -77,7 +79,7 @@ public:
         cells.initialise(box.boxSize, 1 + 0.5*interactionRange);
 
         // Initialise the patchy disc model.
-        PatchyDisc patchyDisc(box, particles, cells,
+        patchyDisc = new PatchyDisc(box, particles, cells,
             maxInteractions, interactionEnergy, interactionRange);
 
         // Initialise random number generator.
@@ -132,150 +134,45 @@ public:
     // Initialise VMMC object.
     vmmc = new vmmc::VMMC(nParticles, dimension, coordinates, orientations,
         0.15, 0.2, 0.5, 0.5, maxInteractions, &boxSize[0], isIsotropic, false, callbacks);
+    std::cout << "Initializer finished.\n";
+
     }
     void execute(){
+        std::cout << "Execute entered.\n";
         for (unsigned int i=0;i<1000;i++)
         {
             // Increment simulation by 1000 Monte Carlo Sweeps.
+            std::cout << "  Particles...\n";
             vmmc += 10*nParticles;
+ 
+            std::cout << "  Trajectory\n";
 
             // Append particle coordinates to an xyz trajectory.
-            if (i == 0) io.appendXyzTrajectory(dimension, particles, true);
-            else io.appendXyzTrajectory(dimension, particles, false);
-
+            if (i == 0) {
+                io.appendXyzTrajectory(dimension, particles, true);
+                std::cout << "  Append True\n";
+            }
+            else {
+                std::cout << "  Append False\n";
+                io.appendXyzTrajectory(dimension, particles, false);
+            }
+            std::cout << "Report\n";
             // Report.
-            printf("sweeps = %9.4e, energy = %5.4f\n", ((double) (i+1)*1000), patchyDisc.getEnergy());
+            printf("sweeps = %9.4e, energy = %5.4f\n", ((double) (i+1)*1000), patchyDisc->getEnergy());
+            std::cout << "End report\n";
         }
 
         std::cout << "\nComplete!\n";
     }
-    int area(){return width * height;}
 };
 
 
 int main(int argc, char** argv)
 {
-    PatchyDiscEnv env (3,4 );
-    // env.set_values(3, 4);
-    std::cout << "area: " << env.area();
-    // -------
-
-    // Simulation parameters.
-    unsigned int dimension = 2;                     // dimension of simulation box
-    unsigned int nParticles = 1000;                 // number of particles
-    double interactionEnergy = 8.0;                 // pair interaction energy scale (in units of kBT)
-    double interactionRange = 0.1;                  // diameter of patch (in units of particle diameter)
-    double density = 0.2;                           // particle density
-    double baseLength;                              // base length of simulation box
-    unsigned int maxInteractions = 3;               // maximum number of interactions per particle (number of patches)
-
-
-
-    // Data structures.
-    std::vector<Particle> particles(nParticles);    // particle container
-    CellList cells;                                 // cell list
-    bool isIsotropic[nParticles];                   // whether the potential of each particle is isotropic
-
-
-    // Resize particle container.
-    particles.resize(nParticles);
-
-    // Work out base length of simulation box (particle diameter is one).
-    if (dimension == 2) baseLength = std::pow((nParticles*M_PI)/(4.0*density), 1.0/2.0);
-    else baseLength = std::pow((nParticles*M_PI)/(6.0*density), 1.0/3.0);
-
-
-    std::vector<double> boxSize;
-    for (unsigned int i=0;i<dimension;i++)
-        boxSize.push_back(baseLength);
-
-    // UP TO HERE
-
-    // Initialise simulation box object.
-    Box box(boxSize);
-
-    // Initialise input/output class,
-    InputOutput io;
-
-    // Create VMD script.
-    io.vmdScript(boxSize);
-
-    // Initialise cell list.
-    cells.setDimension(dimension);
-    cells.initialise(box.boxSize, 1 + 0.5*interactionRange);
-
-    // Initialise the patchy disc model.
-    PatchyDisc patchyDisc(box, particles, cells,
-        maxInteractions, interactionEnergy, interactionRange);
-
-    // Initialise random number generator.
-    MersenneTwister rng;
-
-    // Initialise particle initialisation object.
-    Initialise initialise;
-
-    // Generate a random particle configuration.
-    initialise.random(particles, cells, box, rng, false);
-
-    // Initialise data structures needed by the VMMC class.
-    double coordinates[dimension*nParticles];
-    double orientations[dimension*nParticles];
-
-    // Copy particle coordinates and orientations into C-style arrays.
-    for (unsigned int i=0;i<nParticles;i++)
-    {
-        for (unsigned int j=0;j<dimension;j++)
-        {
-            coordinates[dimension*i + j] = particles[i].position[j];
-            orientations[dimension*i + j] = particles[i].orientation[j];
-        }
-
-        // Set all particles as anisotropic.
-        isIsotropic[i] = false;
-    }
-
-    // Initialise the VMMC callback functions.
-    using namespace std::placeholders;
-    vmmc::CallbackFunctions callbacks;
-#ifndef ISOTROPIC
-    callbacks.energyCallback =
-        std::bind(&PatchyDisc::computeEnergy, patchyDisc, _1, _2, _3);
-    callbacks.pairEnergyCallback =
-        std::bind(&PatchyDisc::computePairEnergy, patchyDisc, _1, _2, _3, _4, _5, _6);
-    callbacks.interactionsCallback =
-        std::bind(&PatchyDisc::computeInteractions, patchyDisc, _1, _2, _3, _4);
-    callbacks.postMoveCallback =
-        std::bind(&PatchyDisc::applyPostMoveUpdates, patchyDisc, _1, _2, _3);
-#else
-    callbacks.energyCallback =
-        std::bind(&PatchyDisc::computeEnergy, patchyDisc, _1, _2);
-    callbacks.pairEnergyCallback =
-        std::bind(&PatchyDisc::computePairEnergy, patchyDisc, _1, _2, _3, _4);
-    callbacks.interactionsCallback =
-        std::bind(&PatchyDisc::computeInteractions, patchyDisc, _1, _2, _3);
-    callbacks.postMoveCallback =
-        std::bind(&PatchyDisc::applyPostMoveUpdates, patchyDisc, _1, _2);
-#endif
-
-    // Initialise VMMC object.
-    vmmc::VMMC vmmc(nParticles, dimension, coordinates, orientations,
-        0.15, 0.2, 0.5, 0.5, maxInteractions, &boxSize[0], isIsotropic, false, callbacks);
-
-    // Execute the simulation.
-    for (unsigned int i=0;i<1000;i++)
-    {
-        // Increment simulation by 1000 Monte Carlo Sweeps.
-        vmmc += 10*nParticles;
-
-        // Append particle coordinates to an xyz trajectory.
-        if (i == 0) io.appendXyzTrajectory(dimension, particles, true);
-        else io.appendXyzTrajectory(dimension, particles, false);
-
-        // Report.
-        printf("sweeps = %9.4e, energy = %5.4f\n", ((double) (i+1)*1000), patchyDisc.getEnergy());
-    }
-
-    std::cout << "\nComplete!\n";
+    PatchyDiscEnv env = PatchyDiscEnv();
+    std::cout << "Beginning execution\n";
+    env.execute();
+    std::cout << "Ending execution\n";
 
     // We're done!
     return (EXIT_SUCCESS);
